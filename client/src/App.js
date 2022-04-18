10b97a9030e9265fd01ca1b99340392b46af8922
michael Means <colemeans51@gmail.com>
2022-04-18 17:05:18 -0500

    save

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import './App.scss';
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout';
+import Layout from './components/Layout'
+import './App.scss'
+import Home from './components/home'
 
 function App() {
   return (
- <>
- <Routes>
-   <Route path="/" element={<Layout />} />
- </Routes>
- </>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
