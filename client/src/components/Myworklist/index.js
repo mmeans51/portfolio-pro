e07722971cf37478ef4fafebe6f2d63ec612b22e
michael Means <colemeans51@gmail.com>
2022-06-28 18:14:30 -0500

    adding my work list

---
+++ client/src/components/Myworklist/index.js
@@ -0,0 +1,26 @@
+import "./MyworkList.css"
+import Mywork from "../Mywork/Mywork"
+import { Work } from "../../data"
+
+const MyworkList = () => {
+    return (
+        <div className="mw">
+            <div className="mw-text">
+                <h1 className="mw-title">My Work</h1>
+                <p className="mw-description">
+                    Below you will see a few screen grabs of some projects i made or contributed too during the coding Boot camp. the images are link to the active webpage.
+                    Also, head over to my git page at github.com/mmeans51 to see my other projects.
+                </p>
+            </div>
+            <div className="mw-list">
+                {Work.map((item) => (
+                    <Mywork key={item.id} img={item.img} link={item.link} />
+                ))}
+              
+            </div>
+            
+        </div>
+    );
+};
+
+export default MyworkList
