54a9251a0fe4bb6611fbc15fa404764a2456093f
michael Means <colemeans51@gmail.com>
2022-07-19 16:36:42 -0500

    fixing about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -38,7 +38,7 @@ const About = () => {
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
             </p>
-            <p align="LEFT">
+            <p>
             I'm quietly confident, naturally curious, and perpetually working on
             improving my chops one design problem at a time.
             </p>
