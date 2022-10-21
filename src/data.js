3e1ab7d56f204d5ea3ef4caccadb688bb0083804
michael Means <colemeans51@gmail.com>
2022-10-21 17:36:18 -0500

    sa

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 import runbuddy from "./img/screencapture-mmeans51-github-io-run-buddy-2022-03-30-16_24_11.jpg"
 import watch from "./img/screencapture-felipezintzun-github-io-watchopedia-2022-03-31-18_59_08.jpg"
 import teksess from "./img/screencapture-teksess-herokuapp-2022-03-30-20_23_09.jpg"
+import TaskmasterPro from "./img/screencapture-mmeans51-github-io-taskmaster-pro-2022-10-20-17_15_22.jpg"
+import noteldenring from "./img/maxresdefault.jpg"
+import foodfest from "./img/screencapture-mmeans51-github-io-food-fest-index-html-2022-10-20-17_55_56.jpg"
 export const Work = [
 
     {
@@ -14,8 +17,23 @@ export const Work = [
         link: "https://felipezintzun.github.io/watchopedia/",
     },
     {
-        id: 2,
+        id: 3,
         img:  teksess ,
         link: "https://teksess.herokuapp.com/",
     },
+    {
+        id: 4,
+        img: TaskmasterPro,
+        link: "https://mmeans51.github.io/taskmaster-pro/",
+    },
+    { 
+        id: 5,
+        img: noteldenring,
+        link: "https://mmeans51.github.io/NotEldenRing/",
+    },
+    {
+        id: 6,
+        img: foodfest,
+        link: "https://mmeans51.github.io/food-fest/index.html#",
+    }
 ];
